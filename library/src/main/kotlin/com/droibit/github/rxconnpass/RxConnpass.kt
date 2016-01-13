e0b449665e8fae7eab6038ea8fe9e2cdde56b759
Shinya Kumagai <roomful.rooms@gmail.com>
2016-01-13 10:59:59 +0900

    First Commit

---
+++ library/src/main/kotlin/com/droibit/github/rxconnpass/RxConnpass.kt
@@ -0,0 +1,5 @@
+package com.droibit.github.rxconnpass
+
+/**
+ * Created by kumagai on 2016/01/13.
+ */
---
+++ sample/src/main/kotlin/com/droibit/github/rxconnpass/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.droibit.github.rxconnpass.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.droibit.github.rxconnpass.R
+
+class MainActivity : AppCompatActivity() {
+
+    /** {@inheritDoc}  */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
