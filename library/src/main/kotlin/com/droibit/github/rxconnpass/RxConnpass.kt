0ab68202b3af437f87611bc285b66cb13ed02d25
Shinya Kumagai <roomful.rooms@gmail.com>
2016-01-16 16:37:23 +0900

    RxConnpassのファクトリメソッドをリファクタリング

--- library/src/main/kotlin/com/droibit/github/rxconnpass/RxConnpass.kt
+++ library/src/main/kotlin/com/droibit/github/rxconnpass/RxConnpass.kt
@@ -21,20 +21,17 @@ public class RxConnpass private constructor(public val service: ConnpassService)
         public fun newClient(client: Call.Factory? = null) = newClient(baseUrl, client)
 
         internal fun newClient(baseUrl: String, client: Call.Factory? = null): RxConnpass {
+            val moshi = Moshi.Builder()
+                             .add(ResponseAdapters.factory)
+                             .build()
 
-            val retrofit = Retrofit.Builder().run {
-                baseUrl(baseUrl)
-                addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                callFactory(client ?: OkHttpClient())
-
-                val moshi = Moshi.Builder().run {
-                    add(ResponseAdapters.factory)
-                    build()
-                }
-                addConverterFactory(MoshiConverterFactory.create(moshi))
-
-                build()
-            }
+            val retrofit = Retrofit.Builder()
+                                   .baseUrl(baseUrl)
+                                   .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                                   .callFactory(client ?: OkHttpClient())
+                                   .addConverterFactory(MoshiConverterFactory.create(moshi))
+                                   .build()
+            
             return RxConnpass(retrofit.create(ConnpassService::class.java))
         }
     }
