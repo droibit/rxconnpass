0eee5ce5bef08277e33cc823e6321e9c5892c2a4
Shinya Kumagai <roomful.rooms@gmail.com>
2016-01-20 00:15:31 +0900

    最低限のDataBinding用のリソースファイルを追加
    実行時クラッシュします。

--- sample/src/main/kotlin/com/droibit/github/rxconnpass/app/view/activity/MainActivity.kt
+++ sample/src/main/kotlin/com/droibit/github/rxconnpass/app/view/activity/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.droibit.github.rxconnpass.app.view.activity
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.droibit.github.rxconnpass.app.R
+import com.droibit.github.rxconnpass.app.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
 
     /** {@inheritDoc}  */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
