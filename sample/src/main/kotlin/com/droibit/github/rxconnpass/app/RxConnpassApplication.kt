259e1cdc2a13e0ce6cd8775d736918d69e65ffc1
Shinya Kumagai <roomful.rooms@gmail.com>
2016-01-20 17:06:00 +0900

    メイン画面のツールバーレイアウトを作成

---
+++ sample/src/main/kotlin/com/droibit/github/rxconnpass/app/RxConnpassApplication.kt
@@ -0,0 +1,15 @@
+package com.droibit.github.rxconnpass.app
+
+import android.app.Application
+
+/**
+ *
+ *
+ * @author kumagai
+ */
+class RxConnpassApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
