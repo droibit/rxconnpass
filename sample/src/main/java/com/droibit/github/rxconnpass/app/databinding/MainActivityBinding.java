8ff5d44d28f0943c41cbb3c57fe584abfbd62074
Shinya Kumagai <roomful.rooms@gmail.com>
2016-01-20 16:24:58 +0900

    javaソースコード用のパッケージを追加

---
+++ sample/src/main/java/com/droibit/github/rxconnpass/app/databinding/MainActivityBinding.java
@@ -0,0 +1,9 @@
+package com.droibit.github.rxconnpass.app.databinding;
+
+/**
+ * @author kumagai
+ */
+public class MainActivityBinding {
+
+    private ActivityMainBinding mBinding;
+}
--- sample/src/main/kotlin/com/droibit/github/rxconnpass/app/view/activity/MainActivity.kt
+++ sample/src/main/kotlin/com/droibit/github/rxconnpass/app/view/activity/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.droibit.github.rxconnpass.app.view.activity
 
-import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.droibit.github.rxconnpass.app.R
-import com.droibit.github.rxconnpass.app.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
 
-    private lateinit var mBinding: ActivityMainBinding
+    //private lateinit var mBinding: ActivityMainBinding
 
     /** {@inheritDoc}  */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+        //mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
