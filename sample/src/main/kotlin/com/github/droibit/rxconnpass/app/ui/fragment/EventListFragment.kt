03fc5b1c61028f9590dd5a11324e531a989e1b1f
Shinya Kumagai <roomful.rooms@gmail.com>
2016-01-27 18:13:00 +0900

    EventListFragmentをActivityに追加

--- sample/src/main/kotlin/com/github/droibit/rxconnpass/app/ui/fragment/EventListFragment.kt
+++ sample/src/main/kotlin/com/github/droibit/rxconnpass/app/ui/fragment/EventListFragment.kt
@@ -1,6 +1,14 @@
 package com.github.droibit.rxconnpass.app.ui.fragment
 
+import android.databinding.DataBindingUtil
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.droibit.rxconnpass.app.R
+import com.github.droibit.rxconnpass.app.databinding.FragmentEventListBinding
 
 /**
  *
@@ -9,4 +17,18 @@ import android.support.v4.app.Fragment
  */
 class EventListFragment: Fragment() {
 
+    private lateinit var binding: FragmentEventListBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_event_list, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recycler.apply {
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
 }
