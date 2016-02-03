f7b19b07392737ea7ea8bba11b563d7454e1d83c
Shinya Kumagai <roomful.rooms@gmail.com>
2016-02-03 14:16:32 +0900

    イベントリストのバインディング処理を修正
    宣言的に開催日と参加者もバインディングしたいのでカスタムBindingを作成して対応した。
    
    カスタムBindingはKolitnのobject＋@JvmStaticではダメだった。
    （xml側でカスタムアトリビュートが見つからない）
    そのためJava側で作成している。

---
+++ sample/src/main/java/com/github/droibit/rxconnpass/app/util/binding/BindingAdapters.java
@@ -0,0 +1,54 @@
+package com.github.droibit.rxconnpass.app.util.binding;
+
+import com.github.droibit.rxconnpass.Event;
+import com.github.droibit.rxconnpass.app.R;
+
+import android.content.Context;
+import android.databinding.BindingAdapter;
+import android.support.annotation.ColorRes;
+import android.support.v4.content.ContextCompat;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+
+import java.util.Date;
+
+/**
+ * Created by kumagai on 2016/02/03.
+ */
+public class BindingAdapters {
+
+    @BindingAdapter({"bind:accepted", "bind:limit"})
+    public static void bindParticipantText(TextView view, int accepted, Integer limit) {
+        final Context context = view.getContext();
+
+        String acceptedText, limitText;
+        boolean overload = false;
+        if (limit != null) {
+            acceptedText = String.valueOf(accepted);
+            limitText = String.valueOf(limit);
+            overload = limit != 0 && accepted >= limit;
+        } else {
+            acceptedText = limitText = context.getString(R.string.empty);
+        }
+        final String text = context.getString(R.string.participant_format, acceptedText, limitText);
+        view.setText(text);
+
+        final int colorRes = textColor(overload);
+        view.setTextColor(ContextCompat.getColor(context, colorRes));
+    }
+
+    @BindingAdapter({"bind:startedAt", "bind:endedAt"})
+    public static void bindDateText(TextView view, Date startedAt, Date endedAt) {
+        final java.text.DateFormat dateFormat = DateFormat.getDateFormat(view.getContext());
+        view.setText(dateFormat.format(startedAt));
+
+        final boolean finished = endedAt.compareTo(new Date()) < 0;
+        final int colorRes = textColor(finished);
+        view.setTextColor(ContextCompat.getColor(view.getContext(), colorRes));
+    }
+
+    @ColorRes
+    private static int textColor(boolean useRedColor) {
+        return (useRedColor) ? R.color.material_red_a200 : R.color.secondary_text_default_material_light;
+    }
+}
