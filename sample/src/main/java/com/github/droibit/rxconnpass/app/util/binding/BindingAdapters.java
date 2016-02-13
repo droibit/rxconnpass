cef80e960adf55ee784804485cee9fd13cc7bc3f
Shinya Kumagai <roomful.rooms@gmail.com>
2016-02-13 20:46:11 +0900

    イベント一覧のレイアウトを修正
    左側にアイコンで日付的にイベントに参加できるかどうかを識別器できるようにしてみた。

--- sample/src/main/java/com/github/droibit/rxconnpass/app/util/binding/BindingAdapters.java
+++ sample/src/main/java/com/github/droibit/rxconnpass/app/util/binding/BindingAdapters.java
@@ -40,14 +40,10 @@ public class BindingAdapters {
     public static void bindDateText(TextView view, Date startedAt, Date endedAt) {
         final java.text.DateFormat dateFormat = DateFormat.getDateFormat(view.getContext());
         view.setText(dateFormat.format(startedAt));
-
-        final boolean finished = endedAt.compareTo(new Date()) < 0;
-        final int colorRes = textColor(finished);
-        view.setTextColor(ContextCompat.getColor(view.getContext(), colorRes));
     }
 
     @ColorRes
     private static int textColor(boolean useRedColor) {
-        return (useRedColor) ? R.color.material_red_a200 : R.color.secondary_text_default_material_light;
+        return (useRedColor) ? R.color.material_red_a100 : R.color.secondary_text_default_material_light;
     }
 }
