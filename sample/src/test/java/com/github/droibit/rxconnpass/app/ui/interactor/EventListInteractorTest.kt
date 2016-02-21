a0cbb943ff18aec069fa4c2acf85a356c0ce92ca
Shinya Kumagai <roomful.rooms@gmail.com>
2016-02-22 00:03:18 +0900

    [未完了] EventListInteractorTestの一部実装
    インターフェースのViewをモックしたためかObservabie#subscribe()時に
    onNextがnullであると言われている。
    Viewをから実装にしないといけないかもしれない？

--- sample/src/test/java/com/github/droibit/rxconnpass/app/ui/interactor/EventListInteractorTest.kt
+++ sample/src/test/java/com/github/droibit/rxconnpass/app/ui/interactor/EventListInteractorTest.kt
@@ -1,5 +1,59 @@
 package com.github.droibit.rxconnpass.app.ui.interactor
 
+import com.github.droibit.rxconnpass.Event
+import com.github.droibit.rxconnpass.app.model.SearchAction
+import com.github.droibit.rxconnpass.app.ui.view.EventListView
+import com.github.droibit.rxconnpass.app.util.RxSchedulersOverrideRule
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnit
+import rx.Observable
+import rx.subscriptions.CompositeSubscription
+
 class EventListInteractorTest {
 
+    @Rule
+    @JvmField
+    val mockito = MockitoJUnit.rule()
+    @Rule
+    @JvmField
+    val overrideSchedulers = RxSchedulersOverrideRule()
+
+    @Mock
+    lateinit var searchAction: SearchAction
+    @Mock
+    lateinit var view: EventListView
+    @Mock
+    lateinit var mockEvents: List<Event>
+
+    lateinit var interactor: EventListInteractor
+
+    @Before
+    fun setup() {
+        interactor = EventListInteractor(action = searchAction,
+                                         compositeSubscription = CompositeSubscription())
+        interactor.init(view)
+    }
+
+    @After
+    fun tearDown() {
+        interactor.onDetach()
+    }
+
+    @Test
+    fun searchByKeyword() {
+        doReturn(Observable.just(mockEvents))
+                .`when`(searchAction)
+                .search("kotlin")
+
+        interactor.searchByKeyword("kotlin")
+        verify(view).showProgress()
+        verify(view).hideProgress()
+        verify(view).showContent()
+        verify(view, never()).showError()
+    }
 }
